662aa23574e6dc6eb2cea4c02569ae58ce6b869d
Nicolas <nicolas.launay.0@gmail.com>
2026-07-13 18:35:50 +0200

    fix: (core/pool) ts typecheck about maybe undefined type

--- packages/core/src/services/pool/fight/pool-fight.service.ts
+++ packages/core/src/services/pool/fight/pool-fight.service.ts
@@ -1,94 +1,83 @@
-import type {Pool} from "../pool.interface.ts";
-import type {PoolTurn} from "./pool-turn.interface.ts";
-import type {PoolFight} from "./pool-fight.interface.ts";
-import type {PoolFighter} from "../distribution/pool-fighter.interface.ts";
+import type { Pool } from "../pool.interface.ts";
+import type { PoolTurn } from "./pool-turn.interface.ts";
+import type { PoolFight } from "./pool-fight.interface.ts";
+import type { PoolFighter } from "../distribution/pool-fighter.interface.ts";
 
 export function organizePoolFights(pool: Pool): PoolTurn[] {
-    let poolTurns: PoolTurn[] = []
+  let poolTurns: PoolTurn[] = [];
 
-    let fighters = [...pool.fighters]
-    const poolSize = pool.fighters.length
+  let fighters = [...pool.fighters];
+  const poolSize = pool.fighters.length;
 
-    if (poolSize % 2 == 0) {
+  if (poolSize % 2 == 0) {
+    const centerFighter: PoolFighter = fighters.pop()!;
+    const tableSize = fighters.length;
 
-        const centerFighter: PoolFighter = fighters.pop()
-        const tableSize = fighters.length
+    for (let turn = 1; turn <= tableSize; turn++) {
+      let turnfights: PoolFight[] = [];
 
-        for (let turn = 1; turn <= tableSize; turn++) {
+      // --- MATCHUP DISTRIBUTION ---
+      // First Matchup
+      turnfights.push({
+        fighter1: centerFighter,
+        fighter2: fighters[poolSize / 2 - 1],
+      });
 
-            let turnfights: PoolFight[] = []
+      // Bulk matchup distribution
+      for (let j = 0; j < poolSize / 2 - 1; j++) {
+        turnfights.push({
+          fighter1: fighters[j],
+          fighter2: fighters[tableSize - 1 - j],
+        });
 
-            // --- MATCHUP DISTRIBUTION ---
-            // First Matchup
-            turnfights.push({
-                fighter1: centerFighter,
-                fighter2: fighters[poolSize/2 - 1],
-            })
+        // console.log(turnfights[j])
+      }
 
-            // Bulk matchup distribution
-            for (let j = 0; j < (poolSize)/2 - 1; j++) {
-                turnfights.push({
-                    fighter1: fighters[j],
-                    fighter2: fighters[tableSize - 1 - j],
-                })
+      poolTurns.push({
+        order: turn,
+        fights: turnfights,
+      });
 
-                // console.log(turnfights[j])
-            }
-
-            poolTurns.push(
-                {
-                    order: turn,
-                    fights: turnfights
-                }
-            )
-
-            fighters = rotateFighters(fighters)
-
-        }
-
-    } else {
-
-        for (let turn = 1; turn <= (poolSize); turn++) {
-
-            let turnfights: PoolFight[] = []
-
-            // --- MATCHUP DISTRIBUTION ---
-            for (let j = 0; j < (poolSize-1) / 2; j++) {
-                turnfights.push({
-                    fighter1: fighters[j],
-                    fighter2: fighters[poolSize-1 - (j+1)],
-                })
-
-                // console.log(turnfights[j])
-
-            }
-
-            poolTurns.push(
-                {
-                    order: turn,
-                    fights: turnfights
-                }
-            )
-
-            fighters = rotateFighters(fighters)
-        }
+      fighters = rotateFighters(fighters);
     }
+  } else {
+    for (let turn = 1; turn <= poolSize; turn++) {
+      let turnfights: PoolFight[] = [];
 
-    return poolTurns
+      // --- MATCHUP DISTRIBUTION ---
+      for (let j = 0; j < (poolSize - 1) / 2; j++) {
+        turnfights.push({
+          fighter1: fighters[j],
+          fighter2: fighters[poolSize - 1 - (j + 1)],
+        });
+
+        // console.log(turnfights[j])
+      }
+
+      poolTurns.push({
+        order: turn,
+        fights: turnfights,
+      });
+
+      fighters = rotateFighters(fighters);
+    }
+  }
+
+  return poolTurns;
 }
 
 function rotateFighters(fighters: PoolFighter[]): PoolFighter[] {
-    const step = 1
+  const step = 1;
 
-    let newFighters: PoolFighter[] = []
+  let newFighters: PoolFighter[] = [];
 
-    for (let i = fighters.length - step; i < fighters.length; i++) {
-        newFighters.push(fighters[i])
-    }
+  for (let i = fighters.length - step; i < fighters.length; i++) {
+    newFighters.push(fighters[i]);
+  }
 
-    for (let i = 0; i < fighters.length - step; i++) {
-        newFighters.push(fighters[i])
-    }
+  for (let i = 0; i < fighters.length - step; i++) {
+    newFighters.push(fighters[i]);
+  }
 
-    return newFighters
+  return newFighters;
 }
