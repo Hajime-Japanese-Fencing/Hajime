a3337e2d9f8ff4823762a39a026494c4c557793c
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2026-07-23 11:38:31 +0200

    refactor: (pool) moved and enhanced test factories with PoolBuilder class

---
+++ packages/core/src/pool/__test__/factories.ts
@@ -0,0 +1,42 @@
+import type { FighterEntry } from "../../fighter.ts";
+import { type PoolFighter, newPoolFighter } from "../distribution/pool-fighter.interface.ts";
+import type { Pool } from "../pool.interface.ts";
+
+export function poolFighterEntryFactory(fighterEntry: Partial<FighterEntry>): FighterEntry {
+  return {
+    id: "1",
+    isSeriesHead: false,
+    club: "Club A",
+    ...fighterEntry,
+  };
+}
+export class PoolBuilder {
+  protected number: number = 1;
+  protected size: number = 3;
+  protected fighters: PoolFighter[] = [
+    newPoolFighter("1"),
+    newPoolFighter("2"),
+    newPoolFighter("3"),
+  ];
+
+  public createPool(): PoolBuilder {
+    return this;
+  }
+
+  public withSize(newSize: number): PoolBuilder {
+    this.size = newSize;
+    this.fighters = [];
+    for (let i = 0; i < this.size; i++) {
+      this.fighters.push(newPoolFighter((i + 1).toString()));
+    }
+    return this.createPool();
+  }
+
+  public toPool(): Pool {
+    return {
+      number: this.number,
+      size: this.size,
+      fighters: this.fighters,
+    };
+  }
+}
