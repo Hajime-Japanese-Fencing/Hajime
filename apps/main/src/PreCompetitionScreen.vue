61b7b747c40e2583952080948babf88ab61030d3
Nicolas <nicolas.launay.0@gmail.com>
2026-08-17 16:42:56 +0200

    build: menu now proposes options according to nb of fighters

--- apps/main/src/PreCompetitionScreen.vue
+++ apps/main/src/PreCompetitionScreen.vue
@@ -3,6 +3,8 @@ import { type SelectorItem, SelectorList } from "@hajime/ui";
 import { Dropdown } from "@hajime/ui";
 import type { DropdownOption } from "@hajime/ui";
 import { ref } from "vue";
+import type { PoolSetup } from "@hajime/core";
+import { calculatePossiblePoolSetups } from "@hajime/core";
 
 const selectors: SelectorItem[] = [
   { id: "1", label: "Pools" },
@@ -11,30 +13,38 @@ const selectors: SelectorItem[] = [
 
 const nbFighters = 12;
 
-function testFunc() {
-  console.log("function called");
-}
-const testTitle: string = "Menu";
-const testOptions: DropdownOption[] = [
-  {
-    label: "option 1",
-    return: testFunc,
-  },
-  {
-    label: "option 2",
-    return: testFunc,
-  },
-  {
-    label: "option 3",
-    return: testFunc,
-  },
-];
+const title = ref("Select a pool repartition");
 
-const title = ref("Default");
+const options: DropdownOption[] = calculatePossiblePoolSetups(nbFighters).map(
+  (poolSetup: PoolSetup) => poolSetupToDropdownOption(poolSetup),
+);
+
+const selectedSetup: PoolSetup | null = ref(null);
+
+// -------------------------------------------
+// FUNCTIONS
+// -------------------------------------------
+function poolSetupToDropdownOption(setup: PoolSetup): DropdownOption {
+  const poolSize = setup.poolGroups[0].poolSize;
+  const nbPools = setup.poolGroups[0].amount;
+  let label = `${nbPools} Pools of ${poolSize} Fighters`;
+
+  if (setup.poolGroups.length > 1) {
+    const poolSize2 = setup.poolGroups[1].poolSize;
+    const nbPools2 = setup.poolGroups[1].amount;
+    label += ` & ${nbPools2} Pools of ${poolSize2} Fighters`;
+  }
+
+  return {
+    label: label + ` (${setup.fightCount} Fights)`,
+    return: () => {
+      return setup;
+    },
+  };
+}
 
 function onDropdownSelect(option: DropdownOption) {
   title.value = option.label;
-  console.log(title);
   option.return();
 }
 </script>
@@ -44,7 +54,8 @@ function onDropdownSelect(option: DropdownOption) {
     <SelectorList :items="selectors" />
     <div class="flex-column justify-between">
       <div>Pool Repartition</div>
-      <Dropdown :title="title" :options="testOptions" @select="onDropdownSelect" />
+      <Dropdown :title="title" :options="options" @select="onDropdownSelect" />
+      <div></div>
     </div>
   </div>
 </template>
