85598cb2c52a4aa692e0a91fca988cf0a8bcebd1
Nicolas <nicolas.launay.0@gmail.com>
2026-08-18 11:22:12 +0200

    build: pre competition screen now shows pool cards according to selected setup

--- apps/main/src/PreCompetitionScreen.vue
+++ apps/main/src/PreCompetitionScreen.vue
@@ -5,13 +5,35 @@ import type { DropdownOption } from "@hajime/ui";
 import { ref } from "vue";
 import type { PoolSetup } from "@hajime/core";
 import { calculatePossiblePoolSetups } from "@hajime/core";
+import { distributeFightersInPools, type FighterEntry } from "@hajime/core";
+import { poolToPoolDetails } from "./poolToPoolDetails.mapper.ts";
+import type { Pool } from "@hajime/core";
+import { RankingDetailBuilder } from "@hajime/ui";
+import { PoolCard } from "@hajime/ui";
+
+// -------------------------------------------
+// INPUTS
+// -------------------------------------------
 
 const selectors: SelectorItem[] = [
   { id: "1", label: "Pools" },
   { id: "2", label: "Bracket", disabled: true },
 ];
+const shouldSeparateClubMembers = false;
+const shouldSeparateSeededCompetitors = false;
 
-const nbFighters = 12;
+const fighters: FighterEntry[] = [
+  { id: "1", isSeeded: true, club: "Paris Kendo Club" },
+  { id: "2", isSeeded: false, club: "Paris Kendo Club" },
+  { id: "3", isSeeded: false, club: "Lyon Kendo Club" },
+  { id: "4", isSeeded: true, club: "Lyon Kendo Club" },
+  { id: "5", isSeeded: false, club: "Marseille Kenshikan" },
+  { id: "6", isSeeded: false, club: "Marseille Kenshikan" },
+  { id: "7", isSeeded: true, club: "Bordeaux Kendo" },
+  { id: "8", isSeeded: false, club: "Bordeaux Kendo" },
+  { id: "9", isSeeded: false, club: "Toulouse Kendo Kai" },
+];
+const nbFighters = fighters.length;
 
 const title = ref("Select a pool repartition");
 
@@ -19,11 +41,14 @@ const options: DropdownOption[] = calculatePossiblePoolSetups(nbFighters).map(
   (poolSetup: PoolSetup) => poolSetupToDropdownOption(poolSetup),
 );
 
-const selectedSetup: PoolSetup | null = ref(null);
+const selectedSetup = ref<PoolSetup>({ poolGroups: [], fightCount: 0 });
+const pools = ref<Pool[]>([]);
 
 // -------------------------------------------
 // FUNCTIONS
 // -------------------------------------------
+
+// TO ISOLATE IN AN "ADAPTER" TYPE FILE PROBABLY ?
 function poolSetupToDropdownOption(setup: PoolSetup): DropdownOption {
   const poolSize = setup.poolGroups[0].poolSize;
   const nbPools = setup.poolGroups[0].amount;
@@ -45,17 +70,33 @@ function poolSetupToDropdownOption(setup: PoolSetup): DropdownOption {
 
 function onDropdownSelect(option: DropdownOption) {
   title.value = option.label;
-  option.return();
+  selectedSetup.value = option.return();
+  pools.value = distributeFightersInPools(
+    fighters,
+    selectedSetup.value,
+    shouldSeparateClubMembers,
+    shouldSeparateSeededCompetitors,
+  );
 }
 </script>
 
+<!--TEMPORARY: rankingDetails names are defined from fighter ids -->
+<!--TODO: need to add name to fighter entry data format-->
 <template>
   <div class="flex gap-4">
     <SelectorList :items="selectors" />
     <div class="flex-column justify-between">
       <div>Pool Repartition</div>
       <Dropdown :title="title" :options="options" @select="onDropdownSelect" />
-      <div></div>
+      <div class="grid">
+        <PoolCard
+          v-for="pool of pools"
+          :pool-details="poolToPoolDetails(pool)"
+          :ranking-details="
+            pool.fighters.map((f) => new RankingDetailBuilder().withName(f.fighter.id).build())
+          "
+        />
+      </div>
     </div>
   </div>
 </template>
